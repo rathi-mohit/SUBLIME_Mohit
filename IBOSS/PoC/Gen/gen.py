b4dab82570228e6bc59046b8a9164b399b5ab2c0
rathi-mohit <mohitrathi2490@gmail.com>
2025-11-23 18:37:56 +0530

    IBOSS PoC fix

---
+++ IBOSS/PoC/Gen/gen.py
@@ -0,0 +1,24 @@
+# Code for generation: Files saved in GitHub
+ 
+import pandas as pd
+import numpy as np
+
+N = 1000         # Define the number of datapoints here
+output_file = "input.csv"
+
+mu_x1, sigma_x1 = 0, 1     # X1: Mean = 0, Std Dev = 1
+mu_x2, sigma_x2 = 0, 10    # X2: Mean = 50, Std Dev = 100
+
+np.random.seed(42) # Seed (for reproducibility)
+
+data = {
+    'X1': np.random.normal(mu_x1, sigma_x1, N),
+    'X2': np.random.normal(mu_x2, sigma_x2, N)
+}
+
+df = pd.DataFrame(data)
+
+df.to_csv(output_file, index=False)
+
+print(f"Successfully generated {N} datapoints and saved to '{output_file}'")
+print(df.head())
