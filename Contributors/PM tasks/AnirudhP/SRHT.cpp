6fe031445f6930af4cea0c52b40ca1b9b449d773
Anirudhmm24 <mm24b022@smail.iitm.ac.in>
2025-12-19 17:47:29 +0530

    Add files via upload

---
+++ Contributors/PM tasks/AnirudhP/SRHT.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <vector>
+#include "LinearRegression.hpp"
+#include <cmath>
+#include <random>
+#include <bit>
+
+using namespace std;
+using namespace Eigen;
+
+void multiply(double *y, int n)
+{
+    if(n<=2048)
+    {
+    for(int h=1; h<n; h=h*2)
+    {
+        for(int i=0; i<n; i+=2*h)
+        {
+            #pragma omp simd
+            for(int j=i; j<i+h; j++)
+            {
+                double s=y[j];
+                double t=y[j+h];
+                y[j]=s+t;
+                y[j+h]=s-t;
+            }
+        }
+    }
+    return;
+    }
+    
+    int h=n/2;
+    multiply(y,h);
+    multiply(y+h,h);
+
+    #pragma omp simd
+    for(int i=0; i<h; i++)
+    {
+        double s=y[i];
+        double t=y[i+h];
+        y[i]=s+t;
+        y[i+h]=s-t;
+    }
+
+}
+
+//redundant, just in case
+void padwithzeroes(MatrixXd& X, VectorXd& y)
+{
+    int n=X.rows();
+    int q=(int)pow(2,ceil(log2(n)));
+
+    if(q==n)
+    {
+        return;
+    }
+    
+    else
+    {
+        X.conservativeResize(q,X.cols());
+        y.conservativeResize(q);
+
+        X.bottomRows(q-n).setZero();
+        y.tail(q-n).setZero();
+    }
+}
+
+VectorXd SRHT(const MatrixXd& A,const VectorXd& b, int r)
+{
+    //I do not multiply by the constant factor sqrt(n/r) since it gets cancelled in the end.
+    int n=A.rows();
+    int d=A.cols();
+    unsigned int n_padded=bit_ceil((unsigned)n);
+    //int r = ceil(max(48*48*d*log(40*n*d)*log(100*100*d*log(40*n*d)),40*d*log(40*n*d)/e));
+    //int r=(int)(d*log(n));
+
+    random_device rd;
+    mt19937 mt(rd());
+    uniform_int_distribution<> dist(0, n_padded-1);
+    uniform_int_distribution<> diag(0, 1);
+    VectorXd diagonal(n);
+    vector<int> S(r);
+
+    for (int i=0; i<r; ++i)
+    {
+        S[i]=dist(mt);
+    }
+    for(int i=0; i<n; ++i)
+    {
+        diagonal(i)=diag(mt);
+    }
+
+    diagonal.array()=2*diagonal.array()-1;
+
+    //processing target vector
+    VectorXd temp_b=VectorXd::Zero(n_padded);
+    temp_b.head(n)=b;
+    temp_b.head(n).array() *= diagonal.array();
+    multiply(temp_b.data(), n_padded);
+    sort(S.begin(), S.end());
+    VectorXd y_f(r);
+    for(int i=0; i<r; i++)
+    {
+        y_f(i)=temp_b(S[i]);
+    }
+
+    //processing the data
+    MatrixXd X_f(r,d);
+    #pragma omp parallel
+    {
+        VectorXd temp=VectorXd::Zero(n_padded);
+    #pragma omp for
+    for(int j=0; j<d; j++)
+    {
+        temp.tail(n_padded-n).setZero();
+        //temp.head(n).array() = A.col(j).array() * diagonal.head(n).array();
+        //temp.tail(n_padded-n).setZero();
+        const double* col_ptr = &A(0, j);
+        double* temp_ptr = temp.data();
+        const double* diag_ptr = diagonal.data();
+
+        for(int i=0; i<n; i++)
+        {
+            temp_ptr[i] = col_ptr[i] * diag_ptr[i];
+        }
+        
+        multiply(temp.data(), n_padded);
+
+        for(int i=0; i<r; i++)
+        {
+            X_f(i,j)=temp(S[i]);
+        }
+    }
+    }
+    return LinearRegression(X_f,y_f);
+}
